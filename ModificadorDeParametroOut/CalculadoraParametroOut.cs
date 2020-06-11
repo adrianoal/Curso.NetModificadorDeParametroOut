2b9e21ef8d140fe8e0554930831fd8ceb17c671c
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-11 12:43:14 -0300

    Exemplo com modificador de parametro Out

---
+++ ModificadorDeParametroOut/CalculadoraParametroOut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadorDeParametroOut
+{
+    class Calculator
+    {
+        public static void Triple(int origin, out int result)// eu passo um parametro de (entrada), e guardo em outro parametro de (saida)
+        {
+            result = origin * 3;
+        }
+
+    }
+}
---
+++ ModificadorDeParametroOut/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ModificadorDeParametroOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10; 
+            int triple;
+            Calculator.Triple(a, out triple); // a (parametro input) triple (parametro Output)
+            Console.WriteLine(triple);
+
+
+        }
+    }
+}
